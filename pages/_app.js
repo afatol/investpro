c851b272c46ab3329951c649a91c25d7c447886d
afatol <anderson.fatol@gmail.com>
2025-05-30 20:30:47 -0300

    _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
-// pages/_app.js
 import '../styles/globals.css'
 import '../styles/layout.css'
+import Head from 'next/head'
 
 /**
- * Aplica CSS global e layout em todas as páginas.
+ * Aplica CSS global e configurações padrão a todas as páginas.
  */
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>InvestPro</title>
+        <meta name="description" content="Plataforma de investimentos simples e segura" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
