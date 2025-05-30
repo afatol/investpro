9d8abc3408f88e2f2c666d91e66debc872af75ba
afatol <anderson.fatol@gmail.com>
2025-05-30 20:30:09 -0300

    _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,3 @@
-// pages/_document.js
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
@@ -6,9 +5,16 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-BR">
         <Head>
+          <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="description" content="InvestPro - Plataforma de investimentos simples e segura" />
+          <link rel="icon" href="/favicon.ico" />
+          {/* Você pode adicionar fontes ou pré-carregamentos aqui */}
         </Head>
-        <body><Main /><NextScript /></body>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
       </Html>
     )
   }
