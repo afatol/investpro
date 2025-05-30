2c6a8713efc346fb551f08d9c521bca796357df5
afatol <anderson.fatol@gmail.com>
2025-05-30 20:34:33 -0300

    logout.js

--- pages/logout.js
+++ pages/logout.js
@@ -1,11 +1,26 @@
-// pages/logout.js
 import { useEffect } from 'react'
 import { supabase } from '../lib/supabaseClient'
+import Layout from '../components/Layout'
 
 export default function Logout() {
   useEffect(() => {
     supabase.auth.signOut().then(() => window.location.href = '/login')
   }, [])
 
-  return <p style={{ textAlign:'center', marginTop:'2rem' }}>Saindo…</p>
+  return (
+    <Layout>
+      <div className="logout-container">
+        <p>Saindo da sua conta...</p>
+
+        <style jsx>{`
+          .logout-container {
+            text-align: center;
+            padding: 3rem 1rem;
+            font-size: 1.2rem;
+            color: #444;
+          }
+        `}</style>
+      </div>
+    </Layout>
+  )
 }
